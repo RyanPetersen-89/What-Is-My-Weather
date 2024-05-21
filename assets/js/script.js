f27ef8771f957c16ca1026d6818ff2b2a13d4114
Ryan Petersen <rp26898@gmail.com>
2024-05-20 17:05:00 -0700

    continuing to work on the entire project to get it functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,40 @@
 const apiKey = "deeddd3f4c6c7872ae67649acfc3e05f";
-const location = `api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`
-const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${apiKey}`;
-const citySearch = document.getElementById('searchbar').value
+
 const submitBtn = document.getElementById('submit-button')
 
 function getApi() {
-  alert('firstline in getApi function');
+  alert('Firstline in getApi Function');
+  const citySearch = document.getElementById('searchbar').value
   console.log(citySearch);
   alert('Before Fetch');
+  
+  const LocationLookUp = `http://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${apiKey}`;
 
-  fetch(apiUrl)
-  .then(function(response){
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data);
-    console.log('ryan');
-  });
+    // Fetching weather data to get lat and lon
+    $.get(LocationLookUp, function(data) {
+        if (data.cod !== 200) {
+            console.error("City not found");
+            alert('Location Not Found')
+            return;
+        }
+        const lat = data.coord.lat;
+        const lon = data.coord.lon;
+
+        const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+        $.get(apiUrl, function(data) {
+            displayForecast(data);
+        }).fail(function(error) {
+            console.error('Error:', error);
+        });
+    }).fail(function(error) {
+        console.error('Error:', error);
+    });
+
+    /////////////////Double check the above function! Continue code below to push fetch data to cards
+
 }
+
+
 
 submitBtn.addEventListener('click', getApi)
